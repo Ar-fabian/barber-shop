59fe352021332c02a7b3f34fb5ac319fca626bf6
Fabian Alvarado <fabianf_1704@hotmail.com>
2022-02-07 16:32:56 -0500

    renombre la carpeta de citas

--- docs/Agendamiento-de-citas-main/js/popUp.js
+++
@@ -1,5 +0,0 @@
-export const popUp = (element, e) =>{
-    e.preventDefault();
-    document.getElementById('containerForm').classList.toggle('show');
-    document.getElementById(element).classList.toggle('down');
-}
